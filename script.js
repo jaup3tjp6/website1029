ca786308b80a3902f6bd99aa5c3de626666aeadb
jaup3tjp6 <102916443+jaup3tjp6@users.noreply.github.com>
2025-05-03 20:07:14 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -171,13 +171,13 @@ document.addEventListener('keydown', (e) => {
 
 // 選單連結點擊事件
 const menuMessages = {
-    '#about': '我們是114級起承轉合起，歡迎您的參觀與指教！',
+    '#about': '114級數位系畢製成果展「啟程轉何」<br>歡迎參觀 !！',
     '#contact': `<div style="text-align:left;">
     <b><a href="https://www.facebook.com/ntcudct114" target="_blank">Facebook</a></b>
     <span style="display:inline-block; width:32px;"></span>
     <b><a href="https://www.instagram.com/ntcudct114?fbclid=IwY2xjawKC0yhleHRuA2FlbQIxMABicmlkETFTeUZlbjQzMGhJb3FBekhtAR4qeV1BG4YGL9jKlj9iAWSLiTV8QF7bDBwpzaqG_XjjCv3PbBGJNugyfexlPw_aem_6KXOGp4Y1a0oUlhsQzwpFQ" target="_blank">Instagram</a></b>
 </div>`,
-    '#help': '本地圖網站可點擊各展區星球，查看展位詳細資訊。點擊下方圓點或「下一區」可切換展區。星球初始為黑色，點擊後會顯示原本顏色。'
+    '#help': '歡迎來探索展區地圖！<br>點點每一顆星球，就能看到展位的詳細介紹。<br>想切換不同展區？只要點下方的圓點或「下一區」按鈕就行囉！<br>一開始星球都是神祕的黑色，點擊後就會亮出它們的真面目！'
 };
 document.querySelectorAll('.menu ul li a').forEach(link => {
     link.addEventListener('click', (e) => {
